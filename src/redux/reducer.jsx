5e373e0af2f5db874c89e7cca4181255f6b1d86d
TrieuND <trieund@ecomobile.vn>
2024-10-07 17:42:59 +0700

    Readme

--- src/redux/reducer.jsx
+++ src/redux/reducer.jsx
@@ -13,7 +13,6 @@ const initState = {
 };
 
 const rootReducer = (state = initState, action) => {
-    console.log(state, action);
     switch (action.type) {
         case 'todoList/AddTask':
             return {
